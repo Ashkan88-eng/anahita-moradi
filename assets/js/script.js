5792688cd2290765cb8a3a54412ab66a7f84d13f
Ashkan88-eng <aidadarbari@gmail.com>
2025-05-24 11:55:01 -0700

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,19 @@ const navtogglefunc=()=> nav.classList.toggle("active");
 navmenubtn.addEventListener("click",navtogglefunc);
 navclosebtn.addEventListener("click",navtogglefunc);
 
+//them toggle variables
+
+const themebtn=document.querySelectorAll('.theme-btn');
+
+
+for(let i=0 ; i<themebtn.length ; i++) {
+themebtn[i].addEventListener('click' , function(){
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    for(let j =0 ; j <themebtn.length; j++){
+        themebtn[j].classList.toggle('light');
+        themebtn[j].classList.toggle('dark');
+    }
+});
+}
